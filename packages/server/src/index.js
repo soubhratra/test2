ad9355b134d895d58019e8b4b54ff1d095189793
soubhratra <soubhratra@gmail.com>
2020-07-18 12:32:07 -0400

    base version

---
+++ packages/server/src/index.js
@@ -0,0 +1,4 @@
+"use strict";
+exports.__esModule = true;
+var common_1 = require("@test2/common");
+console.log(common_1.add(1, 2));
---
+++ packages/server/src/index.ts
@@ -0,0 +1,3 @@
+import { add } from "@test2/common";
+
+console.log(add(1,2));
