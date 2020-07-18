dbb2e2d07b184cdd6a6f0d4253934053db3c7d2d
soubhratra <soubhratra@gmail.com>
2020-07-18 13:06:43 -0400

    excluding dist folders to gitignore

--- packages/common/dist/index.d.ts
+++ packages/common/dist/index.d.ts
@@ -1,1 +1,2 @@
 export declare const add: (a: number, b: number) => number;
+export declare const substract: (a: number, b: number) => number;
--- packages/common/dist/index.js
+++ packages/common/dist/index.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.add = void 0;
+exports.substract = exports.add = void 0;
 exports.add = (a, b) => a + b;
+exports.substract = (a, b) => a - b;
 //# sourceMappingURL=index.js.map
