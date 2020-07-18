b1c08ff651f8c7754ed6d5ee22edfd705a2d2fe6
soubhratra <soubhratra@gmail.com>
2020-07-18 12:56:36 -0400

    using substract method

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,3 +1,5 @@
-import { add } from "@test2/common";
+import { add, substract } from "@test2/common";
 
-console.log(add(1,2));
+console.log("1 + 2 = " + add(1,2));
+
+console.log("5 - 2 = " + substract(5, 2));
